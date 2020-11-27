d1a785372052f2924ebafeee27d4d2c3dd118249
felicitashoermann <43412653+felicitashoermann@users.noreply.github.com>
2020-11-27 22:57:14 +0100

    fix v-on:submit

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,8 +1,8 @@
 <template>
-  <form class="" action="#" method="post">
+  <form class="" action="#" method="post" v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)">
     <input type="text" name="title" value="" placeholder="Book Title" v-model="bookTitle">
     <input type="text" name="author" value="" placeholder="Book Author" v-model="bookAuthor">
-    <button type="submit" name="button" v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)">Add Book</button>
+    <button type="submit" name="button">Add Book</button>
   </form>
 </template>
 
